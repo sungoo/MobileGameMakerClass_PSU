b7407f60be5c5b95c652bfbd792f7f0d795439a1
sungoo <qkrtjddn234@naver.com>
2024-06-17 00:46:56 +0900

    Vector 클래스 만들어보기 과제

--- 6월/0614_Container/0614_Container.cpp
+++ 6월/0614_Container/0614_Container.cpp
@@ -7,10 +7,10 @@ using namespace std;
 #include "Vector.h"
 #include "List.h"
 
-void PrintVec(const vector<int> v) {
-    for (int i = 0; i < v.size(); i++)
-        cout << v[i] << endl;
-}
+//void PrintVec(const vector<int> v) {
+//    for (int i = 0; i < v.size(); i++)
+//        cout << v[i] << endl;
+//}
 
 int main()
 {
@@ -20,16 +20,16 @@ int main()
     //정적배열
     int arr[5] = { 1,2,3,4,5 };
     vector<int> v;
-    //Vector<double> myv;
+    Vector<double> myv;
     ////몇 개의 배열을 만들건지 미리 주문해두기
-    v.reserve(10);
+    myv.Reserve(10);
     //v.resize(1000);//0~999까지 기본값으로 초기화
-    //for (int i = 0; i < 1000; i++) {
-    //    v.push_back(i);
-    //    //cout << v.capacity() << endl;//벡터의 실제 크기
-    //    //cout << v.size() << endl;//현재 유효값이 들어간 벡터의 크기
-    //    //cout << "========================================" << endl;
-    //}
+    for (int i = 0; i < 50; i++) {
+        v.push_back(i);
+        cout << myv.Capacity() << endl;//벡터의 실제 크기
+        cout << myv.Size() << endl;//현재 유효값이 들어간 벡터의 크기
+        cout << "========================================" << endl;
+    }
     ////낭비되는 capacity가 없게 줄이기
     //v.shrink_to_fit();
 
@@ -37,15 +37,30 @@ int main()
     //    cout << v[i] << endl;
     //    
     //}*/
-
-    for (int i = 0; i < 5; i++) {
+    cout << "========================================" << endl;
+    cout << "========================================" << endl;
+    /*for (int i = 0; i < 5; i++) {
         v.push_back(i);
-    }
+    }*/
     for (int i = 0; i < 5; i++) {
-        v.pop_back();
+        myv.PopBack();
     }
+    for (int i = 0; i < myv.Size(); i++) {
+        cout << myv[i] << endl;
+        cout << myv.Capacity() << endl;//벡터의 실제 크기
+        cout << myv.Size() << endl;//현재 유효값이 들어간 벡터의 크기
+        cout << "========================================" << endl;
+    }
+    cout << "========================================" << endl;
+    cout << "========================================" << endl;
     //vector의 clear는 공간을 없애지 않는다. 내용만 삭제함.
     v.clear();
+    for (int i = 0; i < myv.Size(); i++) {
+        cout << myv[i] << endl;
+        cout << myv.Capacity() << endl;//벡터의 실제 크기
+        cout << myv.Size() << endl;//현재 유효값이 들어간 벡터의 크기
+        cout << "========================================" << endl;
+    }
 
     //임시객체(이름없는 객체 : 생성자 호출 후 바로 소멸자 호출됨)
     vector<int>().swap(v);//Effective STL
@@ -55,22 +70,22 @@ int main()
 
 #pragma endregion
 #pragma region List
-    list<int> l;
-    List myL;
+    //list<int> l;
+    //List myL;
 
-    for (int i = 0; i < 5; i++) {
-        l.push_back(i);
-        l.push_front(i);
+    //for (int i = 0; i < 5; i++) {
+    //    l.push_back(i);
+    //    l.push_front(i);
 
-        myL.PushBack(i);
-        //myL.PushFront(i);
-    }
+    //    myL.PushBack(i);
+    //    //myL.PushFront(i);
+    //}
 
-    myL.PushBack(100);
-    myL.Insert(15, myL.FindNode(3));
+    //myL.PushBack(100);
+    //myL.Insert(15, myL.FindNode(3));
 
-    for (int i = 0; i < myL.Size(); i++) {
-        //cout << myL[i] << endl;
-    }
+    //for (int i = 0; i < myL.Size(); i++) {
+    //    //cout << myL[i] << endl;
+    //}
 #pragma endregion
 }
--- 6월/0614_Container/Vector.h
+++ 6월/0614_Container/Vector.h
@@ -6,20 +6,68 @@ class Vector
 	int _size = 0;
 	int _capacity = 0;
 
-	int* _data = nullptr;
+	T* _data = nullptr;
 
 public:
-	Vector();
-	~Vector();//vector 해제는 여기서
+	Vector() {
+		_size = 1;
+		_capacity = 1;
 
-	void Reserve(int capacity);
-	void PushBack(int value);
-	void PopBack();
-	void Claer();
+		_data = new T[1];
+	}
+	~Vector() {//vector 해제는 여기서
+		/*for (int i = 0; i < _capacity; i++) {
+			delete _data[i];
+		}*/
+		delete[] _data;
+	}
+
+	void Reserve(const int capacity = 10) {
+		//원래 벡터의 크기보다 더 작은 capacity 요구시, 무시
+		if (capacity < _capacity)
+			return;
+		//capacity 만큼 새 벡터 만들기
+		T* newVector = new T[capacity];
+		//기존 벡터에 있던 내용들 모두 새 벡터로 옮기기.
+		for (int i = 0; i < _size; i++) {
+			newVector[i] = _data[i];
+		}
+		//기존 벡터의 포인터와 새 벡터의 포인터 서로 교환.
+		T* temp = _data;
+		_data = newVector;
+		newVector = temp;
+		//기존 벡터 정보 (현재는 newVector에 담김) delete
+		delete[] newVector;
+		
+		
+		_capacity = capacity;
+	}
+	void PushBack(T value) {
+		//벡터가 꽉 참. => 1.2배 크기의 새 벡터로 옮기기
+		if (_size == _capacity) {
+			int newCapacity = static_cast<int>(_capacity * 1.2f);
+			Reserve(newCapacity);
+		}
+		//데이터 집어넣기
+		_data[_size] = value;
+		_size++;
+	}
+	void PopBack() {
+		_data[_size - 1] = NULL;
+		_size--;
+	}
+	void Claer() {
+		for (int i = 0; i < _size; i++) {
+			_data[i] = NULL;
+		}
+		_size = 0;
+	}
 
 	const int& Size() { return _size; }
 	const int& Capacity() { return _capacity; }
 
-	int operator[](int index);
+	T operator[](int index) {
+		return _data[index];
+	}
 };
 
