8cc6acd23970ce1a944075f62572dd81642569ec
sungoo <qkrtjddn234@naver.com>
2024-09-27 00:35:16 +0900

    SleepLock

--- Server/Server/GameServer/GameServer.cpp
+++ Server/Server/GameServer/GameServer.cpp
@@ -2,35 +2,90 @@
 #include "AccountManager.h"
 #include "UserManager.h"
 
-void Func1()
+//Lock 구현
+// 1. Spin Lock(Busy Waiting)
+// 2. Sleep 기반 Lock
+// 3. Event 기반 Lock
+// 
+// Spin Lock
+
+class SpinLock
 {
-    for (int32 i = 0; i < 10000; i++)
+public:
+    void lock()
     {
-        UserManager::GetInstance()->Save();
+        //둘의 경합이 동시에 일어났다.
+
+        //check하고 setting하는 작업이 원자적으로 처리되어야 한다.
+        //=> Compare And Swap(CAS)
+
+        bool expected = false; //flag의 예상값은 false
+        bool desired = true; //flag가 true였으면 좋겠다.
+
+        //compare_exchange_strong의 의사코드
+        //if (flag == expected)
+        //{
+        //    expected = flag;
+        //    flag = desired;
+        //    return true;
+        //}
+        //else //flag가 desired와 같다
+        //{
+        //    expected = flag;
+        //    return false;
+        //}
+
+        while (flag.compare_exchange_strong(expected, desired)==false)
+        {
+            expected = false;
+
+            //Sleep Lock
+            this_thread::sleep_for(std::chrono::seconds(5));
+        }
+    }
+
+    void unlock()
+    {
+        flag = false;
+    }
+
+private:
+    atomic<bool> flag = false;
+};
+
+int32 sum = 0;
+SpinLock myLock;
+
+void Add()
+{
+    for (int32 i = 0; i < 1000000; i++)
+    {
+        //lock...flag -> false
+        std::lock_guard<SpinLock> lg(myLock);
+        //lock...flag -> true
+
+        sum++;
     }
 }
 
-void Func2()
+void Sub()
 {
-    for (int32 i = 0; i < 10000; i++)
+    for (int32 i = 0; i < 1000000; i++)
     {
-        AccountManager::GetInstance()->Login();
+        //lock...flag -> true
+        std::lock_guard<SpinLock> lg(myLock);
+        sum--;
     }
 }
 
 int main()
 {
-    AccountManager::Create();
-    UserManager::Create();
-
-    std::thread t1(Func1);
-    std::thread t2(Func2);
+    std::thread t1(Add);
+    std::thread t2(Sub);
 
     t1.join();
     t2.join();
 
-    cout << "Done!" << endl;
+    cout << sum << endl;
 
-    AccountManager::Delete();
-    UserManager::Delete();
 }
