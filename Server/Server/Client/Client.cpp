9e1d22b9aa1fd74929d6d67ffff15cfde9732bc8
sungoo <qkrtjddn234@naver.com>
2024-10-23 15:52:01 +0900

    Socket Programming

--- Server/Server/Client/Client.cpp
+++ Server/Server/Client/Client.cpp
@@ -1,9 +1,59 @@
-﻿// Client.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿#include "pch.h"
 
-#include "pch.h"
+// Socket 만들기
+// - ipv6
+// - port
+// - protocol
+//
+#include <WinSock2.h>
+#include <MSWSock.h>
+#include <WS2tcpip.h>
+
+#pragma comment(lib, "ws2_32.lib")
 
 int main()
 {
-    HelloWorld();
+	//윈속 초기화(ws2_32 라이브러리 초기화)
+	//관련 정보가 wsaData에 채워짐
+	WSAData wsaData;
+	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		return 0;
+
+	//1. Client Socket 만들기 => 서버에 연결 시킬 socket 만들기
+	SOCKET clientSocket = ::socket(AF_INET, SOCK_STREAM, 0);//socket의 번호를 발급
+	if (clientSocket == INVALID_SOCKET)
+	{
+		int32 errCode = ::WSAGetLastError();
+		cout << "Socket ErrorCode : " << errCode << endl;
+		return 0;
+	}
+
+	//본인 = "127.0.0.1"
+	//강사 = "192.168.0.9"
+
+	//2. Server에 어디에 연결할지(Ip 주소 + 포트번호)
+	SOCKADDR_IN serverAddr;//IPv4
+	::memset(&serverAddr, 0, sizeof(serverAddr));//serverAddr 0으로 다 밀어버리기
+	serverAddr.sin_family = AF_INET;//IPv4
+	::inet_pton(AF_INET, "192.168.0.9", &serverAddr.sin_addr);//127.0.01 => Loop back : 자기 PC의 주소
+	serverAddr.sin_port = ::htons(7777); //1~1000 사이 포트는 건들면 안됨
+	//네트워크 정수 표현 => 빅엔디언
+
+	//3. 연결
+	if (::connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+	{
+		int32 errCode = ::WSAGetLastError();
+		cout << "Connect ErrorCode : " << errCode << endl;
+		return 0;
+	}
+
+	while (true)
+	{
+		//... 서버에서 들어온 데이터 분석, 혹은 클라이언트 코드 실행
+		this_thread::sleep_for(1s);
+	}
+
+	//소켓 리소스 반환 및 윈속 종료
+	::closesocket(clientSocket);
+	::WSACleanup();
 }
--- Server/Server/GameServer/GameServer.cpp
+++ Server/Server/GameServer/GameServer.cpp
@@ -1,84 +1,85 @@
 ﻿#include "pch.h"
-#include "AccountManager.h"
-#include "UserManager.h"
 
-#include "ThreadManager.h"
-#include "Lock.h"
-#include "RefCounting.h"
+#include <WinSock2.h>
+#include <MSWSock.h>
+#include <WS2tcpip.h>
 
-using PlayerRef = shared_ptr<class Player>;
-using InventoryRef = shared_ptr<class Inventory>;
-
-class Player
-{
-public:
-	Player() 
-		:_hp(0), _atk(0)
-	{
-		cout << "생성자 호출" << endl;
-	}
-	Player(int hp, int atk)
-		: _hp(hp), _atk(atk)
-	{
-		cout << "타입 변환 생성자 호출" << endl;
-	}
-	virtual ~Player() 
-	{
-		cout << "소멸자 호출" << endl;
-	}
-
-	void Attack()
-	{
-		if (_target != nullptr)
-		{
-			_target->_hp -= _atk;
-		}
-	}
-
-	bool IsDead()
-	{
-		return _hp <= 0;
-	}
-
-public:
-	PlayerRef _target;
-
-	int _hp;
-	int _atk;
-};
-
-class Knight : public Player
-{
-public:
-	int _stamina = 0;
-};
+#pragma comment(lib, "ws2_32.lib")
 
 int main()
 {
-	CoreGlobal::Create();
+	//윈속 초기화(ws2_32 라이브러리 초기화)
+	//관련 정보가 wsaData에 채워짐
+	WSAData wsaData;
+	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		return 0;
 
-	for (int32 i = 0; i < 5; i++)
+	//1. Listener Socket 만들기 => 연결 시도를 받아줄 socket 만들기
+	//AF_INET vs AF_ANET6 ... IPv4 vs IPv6
+	//SOCK_STREAM vs SOCK_DGRAM ... TCP vs UDP
+	// 0 : 자동으로 protocol 설정
+	SOCKET listener = ::socket(AF_INET, SOCK_STREAM, 0);//socket의 번호를 발급
+	if (listener == INVALID_SOCKET)
 	{
-		TM_M->Launch([]()->void
-			{
-				while (true)
-				{
-					Vector<Knight> v(10);
-
-					Map<int32, Knight> m;
-					m[1] = Knight();
-
-					this_thread::sleep_for(500ms);
-				}
-			});
+		int32 errCode = ::WSAGetLastError();
+		cout << "Socket ErrorCode : " << errCode << endl;
+		return 0;
 	}
+
+	//2. 내가 어떻게 받아야할지... Listener에 세팅
+	SOCKADDR_IN serverAddr;//IPv4
+	::memset(&serverAddr, 0, sizeof(serverAddr));//serverAddr 0으로 다 밀어버리기
+	serverAddr.sin_family = AF_INET;//IPv4
+	serverAddr.sin_addr.s_addr = ::htonl(INADDR_ANY);//알아서 IP 주소 써줘
+	serverAddr.sin_port = ::htons(7777); //1~1000 사이 포트는 건들면 안됨
+
+	//3. Listener 세팅
+	if (::bind(listener, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+	{
+		int32 errCode = ::WSAGetLastError();
+		cout << "Bind ErrorCode : " << errCode << endl;
+		return 0;
+	}
+
+	//4. 연결(Listen) 준비
+	//backlog : 대기자
+	if (::listen(listener, 10) == SOCKET_ERROR)
+	{
+		int32 errCode = ::WSAGetLastError();
+		cout << "Listen ErrorCode : " << errCode << endl;
+		return 0;
+	}
+
+	//---------------연결 준비 끝-------------------
 
 	while (true)
 	{
-		Knight* k = xnew<Knight>();
+		//5. Accept
+		SOCKADDR_IN clientAddr;//IPv4
+		::memset(&clientAddr, 0, sizeof(clientAddr));
+		int32 addrLen = sizeof(clientAddr);
 
-		xrelease(k);
+		//listner에게 현제 대기중인 클라이언트를 받아서 정보 추출(client Addr)
+		SOCKET clientSocket = ::accept(listener, (SOCKADDR*)&clientAddr, &addrLen);
+		if (clientSocket == INVALID_SOCKET)
+		{
+			int32 errCode = ::WSAGetLastError();
+			cout << "Accept ErrorCode : " << errCode << endl;
+			return 0;
+		}
+		//---------손님 입장------------
+		char ipAddress[16];
+		::inet_ntop(AF_INET, &clientAddr.sin_addr, ipAddress, sizeof(ipAddress));
+		cout << "Client Connected! Client IP = " << ipAddress << endl;
+
+		//...TODO
+
 	}
 
-	CoreGlobal::Delete();
+	//6. 원속 종료
+	::closesocket(listener);
+	::WSACleanup();
+
+
+	return 0;
 }
