4cb850f77e682b175b42a9cad2fbd29d86fa1db1
sungoo <31647932+sungoo@users.noreply.github.com>
2024-12-06 15:49:31 +0900

    Protobuf

--- Server/Server/GameServer/pch.h
+++ Server/Server/GameServer/pch.h
@@ -4,10 +4,10 @@
 
 #ifdef _DEBUG
 #pragma comment(lib, "ServerCore\\Debug\\ServerCore.lib")
-//#pragma comment(lib, "Protobuf\\Debug\\ServerCore.lib")
+#pragma comment(lib, "Protobuf\\Debug\\libprotobufd.lib")
 #else
 #pragma comment(lib, "ServerCore\\Release\\ServerCore.lib")
-//#pragma comment(lib, "Protobuf\\Release\\ServerCore.lib")
+#pragma comment(lib, "Protobuf\\Release\\libprotobuf.lib")
 #endif // _DEBUG
 
 
