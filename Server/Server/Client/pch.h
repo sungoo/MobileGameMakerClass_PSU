f013baeafa8c0317764e41b43e781878017e59d8
sungoo <31647932+sungoo@users.noreply.github.com>
2024-12-06 14:49:31 +0900

    ProtoBuf

--- Server/Server/Client/pch.h
+++ Server/Server/Client/pch.h
@@ -3,9 +3,11 @@
 #define WIN32_LEAN_AND_MEAN
 
 #ifdef _DEBUG
-#pragma comment(lib, "Debug\\ServerCore.lib")
+#pragma comment(lib, "ServerCore\\Debug\\ServerCore.lib")
+//#pragma comment(lib, "Protobuf\\Debug\\ServerCore.lib")
 #else
-#pragma comment(lib, "Release\\ServerCore.lib")
+#pragma comment(lib, "ServerCore\\Release\\ServerCore.lib")
+//#pragma comment(lib, "Protobuf\\Release\\ServerCore.lib")
 #endif // _DEBUG
 
 
